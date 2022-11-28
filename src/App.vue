694886a1ccbd865a2a9cdc56f9e5e0f5339f2577
Jesus Campos <jesus.campos123@icloud.com>
2022-11-28 01:48:38 -0500

    Add and remove todos

--- src/App.vue
+++ src/App.vue
@@ -7,31 +7,60 @@
   <main class="container">
     <section>
       <form class="add-todo-form">
-        <input type="text" placeholder="Title" />
+        <input v-model="todoTitle" type="text" placeholder="Title" />
         <div>
-          <button>Add</button>
+          <button @click.prevent="addTodo(todoTitle)">Add</button>
         </div>
       </form>
     </section>
 
-    <section>
-      <div class="todo">
-        <p>Todo 1</p>
+    <section v-if="todos.length === 0">
+      <div class="no-todo">
+        <p>You don't have any todo</p>
+      </div>
+    </section>
+    <section v-else>
+      <div v-for="(todo, index) in todos" class="todo">
+        <p>{{ todo }}</p>
         <div>
-          <button class="btn-remove-todo">&times;</button>
+          <button @click="removeTodo(index)" class="btn-remove-todo">
+            &times;
+          </button>
         </div>
       </div>
     </section>
   </main>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      todoTitle: "",
+      todos: [],
+    };
+  },
+
+  methods: {
+    addTodo() {
+      if (this.todoTitle !== "") {
+        this.todos.push(this.todoTitle);
+        this.todoTitle = "";
+      }
+    },
+
+    removeTodo(id) {
+      this.todos = this.todos.filter((_todo, index) => index !== id);
+    },
+  },
+};
+</script>
 
 <style scoped>
 .navbar {
   display: flex;
   align-items: center;
-  background: var(--navbar-color);
+  background: var(--dark-color);
   padding: 15px;
   padding-inline: 20px;
   margin-bottom: 40px;
@@ -62,16 +91,36 @@
   height: 35px;
 }
 
-.todo {
+.no-todo {
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  background: var(--navbar-color);
+  justify-content: center;
+}
+
+.no-todo p {
+  display: flex;
+  align-items: center;
+  background: var(--dark-color);
   padding-inline: 25px;
   border-radius: 5px;
   height: 60px;
   margin-top: 15px;
   border-radius: 10px;
+}
+
+.todo {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: var(--dark-color);
+  padding-inline: 25px;
+  border-radius: 5px;
+  height: 60px;
+  margin-top: 15px;
+  border-radius: 10px;
+  margin-top: 15px;
+  border-radius: 10px;
+  height: 70px;
 }
 
 .btn-remove-todo {
